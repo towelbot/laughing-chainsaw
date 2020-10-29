d87d89437c8906d2f6d530693bd7e9a67440c70d
towelbot <towel.bot@gmail.com>
2020-10-28 17:14:21 -0700

    jsonify for herokufy service account key
    let serviceAccount = require('./serviceAccountKey.json');

--- index.js
+++ index.js
@@ -24,7 +24,18 @@ const roller = new rpgDiceRoller.DiceRoller();
 
 // Initialize firestore
 const admin = require('firebase-admin');
-let serviceAccount = require('./serviceAccountKey.json');
+let serviceAccount = {
+  "type": "service_account",
+  "project_id": "laughing-chainsaw",
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+};
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
